45e40ff281386880cb6a7b9692f6310ce61cc8f8
ayse <aysecnkci@gmail.com>
2023-10-03 14:36:10 +0300

    dsa

---
+++ summer_camp/dene.py
@@ -0,0 +1,6 @@
+############## Bonus Uygulama - List Comprehension ###########################
+
+#1 Write a list comprehension that generates a list of all possible substrings of a given string.
+string = "myth"
+# ['m', 'my', 'myt', 'myth', 'y', 'yt', 'yth', 't', 'th', 'h']
+len(string)
